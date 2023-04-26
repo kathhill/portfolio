79ae0db8ed7745255d3f228dd1102739cbf2b723
Kathy Hill <kath.hill@sbcglobal.net>
2023-04-26 14:10:58 -0700

    feat: Change event target to currentTarget

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,12 @@
 const cards = document.querySelectorAll(".card");
 
 const flipCard = (event) => {
-    event.target.classList.toggle('flip');
+    event.currentTarget.classList.toggle('flip');
 }
 
 cards.forEach((card) => {
     console.log(card);
-    card.addEventListener('click', flipCard, false)
+    card.addEventListener('click', flipCard);    
 });
 
 
